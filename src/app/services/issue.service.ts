26645a0ac881ec87f0e166b606a9aeb7ce88ce86
LaDfBC <george.mausshardt@gmail.com>
2018-09-02 09:08:32 -0500

    Keep up to date, transfer to other computer

--- src/app/services/issue.service.ts
+++ src/app/services/issue.service.ts
@@ -4,16 +4,21 @@ import { Issue, ISSUES } from '../issues/issues';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 
 export class IssueService {
-  constructor(private messageService: MessageService) { }
+  constructor(private http: HttpClient,
+              private messageService: MessageService) { }
+
+  private issuesUrl = 'localhost:5003';
 
   getIssues(): Observable<Issue[]> {
     this.messageService.add('HeroService: fetched Heroes');
-    return of(ISSUES);
+    issuesJson this.http.get<Issue[]>(this.issuesUrl);
   }
 
   getIssue(id: string): Observable<Issue> {
